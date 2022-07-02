8eb8c7d2e9075f7ac2b9d9cca432ed4fc4cc0804
AlexeyPolskiy <flyouss@mail.ru>
2022-07-02 18:28:51 +0500

    homework6 complet

---
+++ task1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int Promt(string msg)
+{
+    System.Console.Write(msg);
+    int numberInt = int.Parse(Console.ReadLine());
+    return numberInt;
+}
+
+int[] FillArray(int[] fillArr)
+{
+    int length = fillArr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        int element = Promt($"Введите элемент {i + 1} ");
+        fillArr[i] = element;
+    }
+    return fillArr;
+}
+
+void PrintArray(int[] printArr)
+{
+    int length = printArr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        System.Console.Write($"{printArr[i]} ");
+    }
+}
+
+int СalculatePositivElement(int[] calcArr)
+{
+    int length = calcArr.Length;
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (calcArr[i] > 0) sum++;
+    }
+    return sum;
+}
+
+int numberElementMassiv = Promt("Введите число элементов массива => ");
+
+int[] array = new int[numberElementMassiv];
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+int sum = СalculatePositivElement(array);
+System.Console.WriteLine($"число положительных элементов массива = {sum}");
---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double Promt(string msg)
+{
+    System.Console.Write(msg);
+    double numberInt = double.Parse(Console.ReadLine());
+    return numberInt;
+}
+
+double b1 = Promt("Введите число b1 => ");
+double k1 = Promt("Введите число k1 => ");
+double b2 = Promt("Введите число b2 => ");
+double k2 = Promt("Введите число k2 => ");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k2 * x + b2;
+System.Console.WriteLine($"Точка пересечения двух прямых y = k1 * x + b1, y = k2 * x + b2 имеет координаты ({x:f1};{y:f1})");
